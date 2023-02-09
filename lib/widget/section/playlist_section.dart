20d2138ced1e3af1c21a2c1c7d5d0532b24fcdb6
Eddington524 <iamsam8687@gmail.com>
2023-02-09 15:33:21 +0900

    remove: 인기 플레이 카드 분리, listview 추가

--- lib/widget/section/playlist_section.dart
+++ lib/widget/section/playlist_section.dart
@@ -1,7 +1,7 @@
+import 'package:cola/widget/item/play_item.dart';
 import 'package:cola/widget/section/section.dart';
 import 'package:flutter/material.dart';
 
-import 'package:cola/widget/item_image.dart';
 import 'package:cola/widget/item_title_row.dart';
 
 class PlaylistSection extends StatefulWidget {
@@ -21,48 +21,24 @@ class _PlaylistSectionState extends State<PlaylistSection> {
           isFirstBold: false,
           secondText: '인기 플레이',
         ),
-        Row(
-          children: [
-            ///인기 플레이 카드!
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(45),
-              ),
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  const ItemImage(
-                      width: 90,
-                      height: 150,
-                      imgUrl:
-                      'https://w0.peakpx.com/wallpaper/954/59/HD-wallpaper-slam-dunk-10-anime-bastquetball-slam-dunk.jpg',
-                      isCicle: false),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    margin: const EdgeInsets.only(bottom: 10),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      gradient: const LinearGradient(
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                        colors: [
-                          Colors.lightBlueAccent,
-                          Colors.orangeAccent,
-                        ],
-                      ),
-                    ),
-                    child: const ItemImage(
-                        width: 30,
-                        imgUrl:
-                        'https://avatars.githubusercontent.com/u/56524770?v=4',
-                        isCicle: true),
-                  ),
-                ],
-              ),
-            ),
-          ],
+        Container(
+          height: 150,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            children: const <Widget> [
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+              PlayItem(),
+            ],
+          ),
         )
       ],
     ));
