803e118ed843c08aada58051570f722f65b06225
Eddington524 <iamsam8687@gmail.com>
2023-02-10 18:55:01 +0900

    add: 오늘의 인기플레이 listview 적용

---
+++ lib/widget/listview_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import 'item_image.dart';
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  final List<String> imgArr = [
+    'https://w0.peakpx.com/wallpaper/954/59/HD-wallpaper-slam-dunk-10-anime-bastquetball-slam-dunk.jpg',
+    'https://w0.peakpx.com/wallpaper/954/59/HD-wallpaper-slam-dunk-10-anime-bastquetball-slam-dunk.jpg',
+    'https://w0.peakpx.com/wallpaper/954/59/HD-wallpaper-slam-dunk-10-anime-bastquetball-slam-dunk.jpg',
+    'https://w0.peakpx.com/wallpaper/954/59/HD-wallpaper-slam-dunk-10-anime-bastquetball-slam-dunk.jpg',
+    'https://w0.peakpx.com/wallpaper/954/59/HD-wallpaper-slam-dunk-10-anime-bastquetball-slam-dunk.jpg',
+  ];
+
+  final List<String> profilArr = [
+    'https://avatars.githubusercontent.com/u/56524770?v=4',
+    'https://avatars.githubusercontent.com/u/56524770?v=4',
+    'https://avatars.githubusercontent.com/u/56524770?v=4',
+    'https://avatars.githubusercontent.com/u/56524770?v=4',
+    'https://avatars.githubusercontent.com/u/56524770?v=4'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: ListView.separated(
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(width: 10);
+          },
+        itemCount: imgArr.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, int index) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(45),
+            ),
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                ItemImage(
+                    width: 90,
+                    height: 150,
+                    imgUrl: imgArr[index],
+                    isCicle: false),
+                Container(
+                  width: 40,
+                  height: 40,
+                  margin: const EdgeInsets.only(bottom: 10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Colors.lightBlueAccent,
+                        Colors.orangeAccent,
+                      ],
+                    ),
+                  ),
+                  child: ItemImage(
+                      width: 30,
+                      imgUrl: profilArr[index],
+                      isCicle: true),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/widget/section/playlist_section.dart
+++ lib/widget/section/playlist_section.dart
@@ -1,4 +1,5 @@
 import 'package:cola/widget/item/play_item.dart';
+import 'package:cola/widget/listview_page.dart';
 import 'package:cola/widget/section/section.dart';
 import 'package:flutter/material.dart';
 
@@ -15,32 +16,17 @@ class _PlaylistSectionState extends State<PlaylistSection> {
   @override
   Widget build(BuildContext context) {
     return Section(child: Column(
-      children: [
-        const ItemTitle(
+      children: const [
+        ItemTitle(
           firstText: '오늘의 ',
           isFirstBold: false,
           secondText: '인기 플레이',
         ),
-        Container(
-          height: 150,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            shrinkWrap: true,
-            children: const <Widget> [
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-              PlayItem(),
-            ],
-          ),
-        )
+        SizedBox(
+            height: 150,
+            child: ListViewPage(),),
       ],
-    ));
+    ),
+    );
   }
 }
