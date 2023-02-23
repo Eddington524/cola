b26c3b6e41bfacff55a81a3dd8362fea82623aa5
Eddington524 <iamsam8687@gmail.com>
2023-02-23 17:15:00 +0900

    add: banner 슬라이더 pageview 생성

---
+++ lib/model/Banner.dart
@@ -0,0 +1,13 @@
+class Banner {
+  final int id;
+  final String imgUrl;
+
+  Banner(this.id, this.imgUrl);
+}
+
+//sample data
+List<Banner> bannerList = [
+  Banner(0, 'https://cdn.pixabay.com/photo/2018/04/26/16/31/marine-3352341__480.jpg'),
+  Banner(1, 'https://cdn.pixabay.com/photo/2020/05/31/16/53/bookmarks-5243253__480.jpg'),
+  Banner(2, 'https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645_1280.jpg'),
+];
--- lib/pages/home/tab/town.dart
+++ lib/pages/home/tab/town.dart
@@ -1,3 +1,4 @@
+import 'package:cola/widget/banner_slider.dart';
 import 'package:cola/widget/section/auction_section.dart';
 import 'package:cola/widget/section/market_section.dart';
 import 'package:cola/widget/section/news_section.dart';
@@ -25,44 +26,7 @@ class _TownScreenState extends State<TownScreen> {
           child: Column(
             children: [
               ///베너
-              Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  margin: EdgeInsets.only(bottom: 10),
-                  height: 180,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                          height: 150,
-                          child: Stack(
-                            fit: StackFit.expand,
-                            children: [
-                              Container(
-                                margin: EdgeInsets.only(top: 10),
-                                // padding: EdgeInsets.all(10),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  child: Image.network(
-                                      'https://d.line-scdn.net/stf/linecorp/ko/pr/Chef_Banner_ko.jpg',
-                                      fit: BoxFit.cover),
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.topCenter,
-                                child: Container(
-                                  width: 20,
-                                  height: 20,
-                                  color: Colors.purple,
-                                ),
-                              )
-                            ],
-                          )),
-                      Expanded(
-                          child: Container(
-                        child: Text('.......'),
-                      ))
-                    ],
-                  )),
+              BannerSliderView(),
 
               TrendSection(),
 
